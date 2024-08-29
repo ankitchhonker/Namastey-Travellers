1e41954afa594fc350e5da00fa18ceaf7ea897fd
Ankit chhonker <160047114+ankitchhonker@users.noreply.github.com>
2024-08-29 11:56:12 +0530

    Update Holdings.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -15,7 +15,7 @@ const Holdings = () => {
     ]
   }
   useEffect(()=>{
-    axios.get("http://localhost:3002/allHoldings").then((res)=>{
+    axios.get("https://stock-trading-project-1.onrender.com/allHoldings").then((res)=>{
       setAllHoldings(res.data);
     }).catch((err)=>{console.log(err);})
   })
